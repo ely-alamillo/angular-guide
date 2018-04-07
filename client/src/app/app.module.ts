4bfdd0fb8fe19b32e338c2655652c0bec4ad20b6
Ely Alamillo <ely0013@gmail.com>
2018-04-07 14:13:04 -0500

    added servers component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  // this is where we register our components
+  declarations: [AppComponent, ServerComponent, ServersComponent],
+  // allows us to use other modules, works when we split modules
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
