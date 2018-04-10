dc84c923d47b7bb7e90b6567a0de1dc45c596ca0
Ely Alamillo <ely0013@gmail.com>
2018-04-10 09:37:15 -0500

    added formModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+// so we can use [(ngModule)] on forms
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { AppComponent } from "./app.component";
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from "./servers/servers.component";
 
 @NgModule({
   // this is where we register our components
   declarations: [AppComponent, ServerComponent, ServersComponent],
   // allows us to use other modules, works when we split modules
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
