83517cd8ac14916996751aac5387b4d5229058fd
Ely Alamillo <ely0013@gmail.com>
2018-04-10 09:10:29 -0500

    working with data binding

--- client/src/app/server/server.component.ts
+++ client/src/app/server/server.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core'
+import { Component } from "@angular/core";
 
 // decorator to give our class
 // what it needs
 @Component({
   // html class we will access
-  selector: 'app-server',
+  selector: "app-server",
   // points to the file
-  templateUrl: './server.component.html' 
+  templateUrl: "./server.component.html"
 })
 // the class we will need
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = "offline";
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
