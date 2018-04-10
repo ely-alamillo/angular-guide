ddeb8b31d43884ca119628eb482421e4662ca379
Ely Alamillo <ely0013@gmail.com>
2018-04-10 14:16:46 -0500

    worked through directives

--- client/src/app/server/server.component.ts
+++ client/src/app/server/server.component.ts
@@ -6,14 +6,22 @@ import { Component } from "@angular/core";
   // html class we will access
   selector: "app-server",
   // points to the file
-  templateUrl: "./server.component.html"
+  templateUrl: "./server.component.html",
+  styleUrls: ["./styles.css"]
 })
 // the class we will need
 export class ServerComponent {
   serverId = 10;
   serverStatus = "offline";
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+  }
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === "online" ? "green" : "red";
   }
 }
