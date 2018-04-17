9d91ab85aebcd4299b0bcae995ddc2d7b4448b5f
Ely Alamillo <ely0013@gmail.com>
2018-04-17 16:08:46 -0500

    added canDeactivate guard

--- client-5-routing/src/app/app.module.ts
+++ client-5-routing/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 import { AppRoutingModule } from "./app-routing.module";
 import { AuthGuard } from "./auth-guard.service";
 import { AuthService } from "./auth.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 
 @NgModule({
   declarations: [
@@ -29,7 +30,7 @@ import { AuthService } from "./auth.service";
     PageNotFoundComponent
   ],
   imports: [BrowserModule, FormsModule, HttpModule, AppRoutingModule],
-  providers: [ServersService, AuthGuard, AuthService],
+  providers: [ServersService, AuthGuard, AuthService, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
